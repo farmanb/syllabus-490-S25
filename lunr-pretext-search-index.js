d04d731992c21994a8fac6fbffffce38b2b1e01f
blake-farman <blake.farman@gmail.com>
2026-01-06 17:33:46 -0600

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,95 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter",
+  "level": "1",
+  "url": "frontmatter.html",
+  "type": "Front Matter",
+  "number": "",
+  "title": "Front Matter",
+  "body": "  Blake Farman Mathematics and Statistics Louisiana Tech University        copyright   "
+},
+{
+  "id": "contact_information",
+  "level": "1",
+  "url": "contact_information.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Contact Information",
+  "body": " Contact Information      Professor  Dr. Blake Farman    Phone Number  318.257.2951    Email Address   bfarman@latech.edu    Website      Office  Nethken 235       Office Hours     Mon\/Wed\/Fri  10:00 AM  11:00 AM  1:00 PM  3:20 PM       Preferred Method of Communication  The best way to communicate with me during the semester is through email. I monitor my email during regular business hours and try to respond within one business day.    Official University Email Addresses  The University provides each student with an email address and all official course correspondence will only be conducted using official university email addresses.    Email Etiquette  When you send an email, your message should contain the following information   Subject  A few words that describe the content of your email along with the course identifier (Math 4903 001)    Salutation  A professional greeting such as Dear or Hello followed by the appropriate honorific and the recipient's last name (  Dr. X).   If the person you are addressing holds a doctorate ( a PhD ), then you should address the recipient as Dr. X, never Mr. X, Ms. X, or Mrs. . X.    If you are unsure, Professor X is generally a safe way to address a faculty member.       Body  A concise message that clearly expresses the purpose of your email.    Signature  A simple closing (e.g. Sincerely,  Thank you,  Best regards,  ) followed by your first and last name.   For your convenience, included below is an example email.   Sample Email  Subject: Meeting - Math 4903 001  Dear Dr. Farman,  I am having trouble with this week's homework. I have tried working through the examples in the text, but I am struggling to understand the concepts involved. Could we schedule a time to meet before the next class to discuss some of the concepts in more detail?  Thank you,  Jane Doe    "
+},
+{
+  "id": "course_information",
+  "level": "1",
+  "url": "course_information.html",
+  "type": "Section",
+  "number": "2",
+  "title": "Course Information",
+  "body": " Course Information   Course Description  This course provides a hands-on introduction to formal verification and computer-assisted proof development using the Lean theorem prover. You will learn how to represent mathematical concepts in a precise formal language, construct and verify proofs interactively, and leverage automated techniques to eliminate logical errors. Along the way, you’ll explore core ideas from logic, type theory, and proof strategies that can be applied to create robust formalizations of everything from classical theorems to complex software specifications.    Course Prerequisites  A grade of C or better in MATH 307, MATH 311, or CSC 220.    Student Learning Outcomes  Mathematics students will see how formalizing arguments in Lean can reveal hidden assumptions, refine logical thinking, and spark fresh insights, even for well known theorems. Lean also serves as a platform for cutting edge mathematical research, with extensive libraries of verified results that encourage meaningful contributions.  Computer Science students will discover how Lean’s type theoretic foundations apply to verifying algorithms, cryptographic protocols, and large scale software systems, reinforcing a correctness first mindset essential to modern development.  By the end of the course, you will be prepared to produce fully machine checked proofs, apply formal methods to real world challenges, and collaborate effectively with mathematicians and software engineers alike, bridging the gap between theoretical vision and practical reliability.    Course Topics  This course provides an introduction to formal verification and computer-assisted proof development using Lean. Students will explore the following topics    Understanding Mathematical Statements: Learning how to write and check logical statements and rules in Lean.     Building Proofs Step by Step: Using tools and techniques to create clear and logical arguments.     Defining and Working with Data: Exploring how to represent data like numbers, lists, and trees in Lean.     Writing and Reasoning About Programs: Understanding how to write small programs and prove they work correctly.     Handling Complex Computations with Monads: Simplifying advanced programming tasks using special tools in Lean.     Checking Algorithms: Ensuring common algorithms, like sorting, are correct and reliable through formal proofs.     Foundations of Logic and Mathematics: Exploring the deeper structure of logic, types, and mathematical rules.       Instructional Methods  This course is offered as a face-to-face course. Learning will be facilitated through traditional lecture, group work\/activities, homework, and in-class assessments.   "
+},
+{
+  "id": "course_topics-2",
+  "level": "2",
+  "url": "course_information.html#course_topics-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Understanding Mathematical Statements: Building Proofs Step by Step: Defining and Working with Data: Writing and Reasoning About Programs: Handling Complex Computations with Monads: Checking Algorithms: Foundations of Logic and Mathematics: "
+},
+{
+  "id": "evaluation",
+  "level": "1",
+  "url": "evaluation.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Evaluation",
+  "body": " Evaluation   Participation and Engagement  Active participation is essential for success in this course. Students are expected to attend all classes, contribute to discussions, and engage in collaborative activities. This includes asking questions, sharing insights, and working effectively during in-class exercises or group work.  Constructive engagement allows students to deepen their understanding of the material, learn from their peers, and build essential problem-solving and communication skills that will be valuable in future coursework and careers.    Homework  Homework assignments are a critical component of the course. They are designed to reinforce the concepts covered in lectures and provide hands-on experience using Lean. Assignments will include a mix of logical proofs, programming tasks, and conceptual exercises. Solutions must be submitted on time and will be graded for clarity and correctness. Collaboration is encouraged, but each student must submit their own work.    Projects   Projects are a significant component of the course, giving students the opportunity to apply their knowledge to formalize and verify mathematical or computational concepts in Lean. These projects will build problem-solving skills, encourage creativity, and allow students to explore the potential of interactive theorem proving. Both the midterm and final projects will be evaluated based on technical accuracy, clarity of exposition, creativity, and teamwork.    Midterm Project  Students will work in small groups to formalize a specific problem or proof in Lean. The midterm project will focus on a foundational topic, with structured guidance provided to ensure students develop proficiency in key skills.     Final Project  The final project will allow students to choose from a list of complex problems that align with their interests and demonstrates their mastery of course material. Projects will require students to design and implement creative solutions, formalize their work in Lean, and present their work to the class.      Quizzes  Quizzes will be administered periodically to assess understanding of key concepts and ensure students are keeping up with the material. These will be short, focused assessments designed to reinforce learning and identify areas for improvement. Quizzes may include logical proofs, questions about Lean syntax, or problem-solving tasks. They are an opportunity to practice applying course concepts and receive feedback on your progress.    Weights  Grades will be calculated using the weights below.   Evaluation Criteria    Grade Category  Weight    Participation and Engagement  10%    Homework  30%    Midterm Project  20%    Quizzes  10%    Final Project  30%       Grading Scale  Letter grades will be assigned based on the scale below.   Grading Scale    Average  Letter Grade     A     B     C     D     F      "
+},
+{
+  "id": "policies",
+  "level": "1",
+  "url": "policies.html",
+  "type": "Section",
+  "number": "4",
+  "title": "Course Policies and Procedures",
+  "body": " Course Policies and Procedures   At a minimum, all policies stated in the current Louisiana Tech University Student Handbook should be followed. Additional class policies follow.    Textbook   Required Textbook  The required texts for this course are    The Hitchhiker's Guide to Logical Verification by Anne Baanen, Alexander Bentkamp, Jasmin Blanchette, Johannes Hölzl, and Jannis Limperg. This text is freely available in PDF format .     Theorem Proving in Lean 4 by Jeremy Avigad, Leonardo de Moura, Soonho Kong and Sebastian Ullrich, with contributions from the Lean Community. This text is freely available from HTML format .     Mathematics in lean by Jeremy Avigad and Patrick Massot. This text is freely available from HTML format and PDF format .        Attendance Policy  Per University Policy 2206 :    All students are expected to attend classes in which they are enrolled regularly and punctually. Failure to do so may jeopardize a student's scholastic standing and may lead to suspension from the College or University.  The student is responsible for making arrangements satisfactory to the instructor regarding absences including prior notification of planned absences, providing documentation if needed, and making up of examinations and work as required. For other emergency absences, the student shall submit excuses for class absences to the appropriate instructor within three class days following the student's return to their respective class.    As noted above, students are obligated to complete all assigned work promptly, to attend class regularly, and to participate in whatever class discussion may occur. Any behavior that interferes with the ability of other students to learn is unacceptable. If any of these become an issue, you will be asked to leave class for the day.  Your phone should be on silent and put away during class. Headphones should also be put away during class. Texting and browsing social media during class is rude and disrespectful. Please refrain from using your phone during class unless explicitly stated otherwise. If you are caught with a cell phone on your desk, in your lap, or anywhere in view while taking a quiz or exam, it will be treated as a case of academic dishonesty. Smart watches are also prohibited during exams; please put them away during those times.    Late Policy  Late work will not be accepted. Students who cannot submit work due to a University-related activity requiring an absence from class must arrange for work to be submitted before leaving.    Make-up Policy  Make-up opportunities will only be granted in cases of documented emergencies or university-approved absences (e.g., illness, family emergencies, or official university activities). Except in extraordinary circumstances, students must notify the instructor before the missed deadline or class. Students who need to make up work due to a University-related activity requiring an absence from class must arrange for work to be completed before leaving.    Academic Honor Code  In accordance with the Academic Honor Code , students pledge the following:    Being a student of higher standards, I pledge to embody the principles of academic integrity.    Any suspected case of academic dishonesty will be reported. If it is determined that academic misconduct has occurred, the student will receive a grade of F for the course and may be dismised from the University.   "
+},
+{
+  "id": "student_services",
+  "level": "1",
+  "url": "student_services.html",
+  "type": "Section",
+  "number": "5",
+  "title": "Student Services",
+  "body": " Student Services   Accommodations for Students with Disabilities  Students needing testing or classroom accommodations based on a disability must be registered with the Louisiana Tech University Office of Testing & Disability Services. Students are encouraged to discuss those needs with the instructor as soon as possible. If you do not have a current accommodation memo, visit the Office of Testing & Disability Services for assistance.    Counseling Services  Students have access confidential counseling services at no additional cost. Visit the Louisiana Tech Counseling Services website to learn more.    Non-Discrimination  Louisiana Tech University is committed to the principle of providing the opportunity for learning and development of all qualified citizens without regard to race, color, national origin, religion, age, sex, sexual orientation, marital status, or disability. Persons with concerns may contact the Louisiana Tech Director of Title IX Compliance:        Mortissa D. Harvey    Phone  318.257.5911    Office  Wyly Tower 1536    Email  mharvey@latech.edu      Section 504 is a federal law designed to protect the rights of individuals with disabilities in programs and activities that receive Federal financial assistance from the U.S. Department of Education. Persons with concerns may contact the Section 504 Coordinator:        Annie Jantz    Phone:  318.257.2445    Office:  Division of Student Affairs; Keeny Hall 305    Email:  ajantz@latech.edu        Emergency Notification System ( ENS )  All Louisiana Tech students are strongly encouraged to enroll and update their contact information in the Emergency Notification System. It takes just a few seconds to ensure you’re able to receive important text and voice alerts in the event of a campus emergency.    Drop with a W date  The latest date a student may drop a course (or resign his\/her entire schedule) with a W grade for Spring 2025 is Friday, May 2, 2025.    Canvas and Workday Help  Direct questions\/problems to the Computing Center Help Desk ( helpdesk@latech.edu or 318-257-5300).   "
+},
+{
+  "id": "schedule",
+  "level": "1",
+  "url": "schedule.html",
+  "type": "Section",
+  "number": "6",
+  "title": "Schedule",
+  "body": " Schedule    March 12 March 14     March 12  Introduction and Setup    March 14  Terms and Types  Theorem Proving in Lean, Ch. 2   Hitchhiker's Guide to Logical Verification, 1.1, 1.2          March 17 March 21     March 17  Definitions, Variables, Sections, Namespaces  Theorem Proving in Lean, Ch. 2       March 19  Logic, Structured Proofs  Theorem Proving in Lean, Ch. 3   Hitchhiker's Guide to Logical Verification, 4.1, 4.2      March 21  Classical Logic  Theorem Proving in Lean, Ch. 3   Hitchhiker's Guide to Logical Verification, 4.1, 4.2         March 24 March 28     March 24  Universal Quantifiers  Theorem Proving in Lean, Ch. 4    Hitchhiker's Guide to Logical Verification, 4.3       March 26  Existential Quantifiers  Theorem Proving in Lean, Ch. 4    Hitchhiker's Guide to Logical Verification, 4.3       March 28  Tactics  Theorem Proving in Lean, Ch. 5    Hitchhiker's Guide to Logical Verification, 3.1 3.8          March 31 April 4     March 31  Basic Inductive Types  Theorem Proving in Lean, Ch. 7    Hitchhiker's Guide to Logical Verification, 5.1  5.4       April 2  Constructing the Natural Numbers  Theorem Proving in Lean, Ch. 7     Mathematics in Lean, 5.2        April 4  Lists    Theorem Proving in Lean, Ch. 7     Hitchhiker's Guide to Logical Verification, 5.1  5.4          April 7 April 11     April 7     Structures   Theorem Proving in Lean, Ch. 9    Hitchhiker's Guide to Logical Verification, 5.5       Type Classes    Theorem Proving in Lean, Ch. 10     Hitchhiker's Guide to Logical Verification, 5.6          April 9  Sets   Mathematics in Lean, 4.1       April 11  Sets   Mathematics in Lean, 4.1          April 14 April 18     April 14  Set Functions    Mathematics in Lean, 4.2        April 16  Set Functions    Mathematics in Lean, 4.2        April 18  No Classes (University Closed for Easter)       April 21 April 25     April 21  No Classes (University Closed for Easter)    April 23   Algebraic Structures     April 23  Algebraic Structures in Lean I (Operations and Monoids)    Mathematics in Lean, 6.2           April 28 May 2     April 28  Class Cancelled (Jury Duty)    April 31  Algebraic Structures in Lean II (Groups)    Mathematics in Lean, 6.2        May 2  Final Project proposals       May 5 May 9     May 5  Algebraic Structures in Lean II (Rings and Fields)    Mathematics in Lean, 6.2        May 7  Morphisms (Monoids and Groups)    Mathematics in Lean, 8.1.1  Mathematics in Lean, 8.1.2        May 9  Morphisms (Rings)    Mathematics in Lean, 8.2.1           May 12 May 16     May 12  Final Project Working Session    May 14  Final Project Working Session    May 16  Final Project Working Session       May 19     May 19  Final Project Presentation        The instructor reserves the right to modify the schedule as needed.    "
+},
+{
+  "id": "schedule-13-1",
+  "level": "2",
+  "url": "schedule.html#schedule-13-1",
+  "type": "Warning",
+  "number": "6.1",
+  "title": "",
+  "body": " The instructor reserves the right to modify the schedule as needed.  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
